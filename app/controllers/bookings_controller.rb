f95f559bd2611e768ab8f9d59091e3c278fcdb6c
Antoine Mariani <antoine.mariani@me.com>
2023-01-16 10:35:28 +0100

    Setting up routes with controller empty methods, and corresponding view templates

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+
+  def index; end
+  def show; end
+  def new; end
+  def create; end
+  def confirmation; end
+  def edit; end
+  def update; end
+  def destroy; end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date,
+                                    :starts_at,
+                                    :people,
+                                    :gender,
+                                    :first_name,
+                                    :last_name,
+                                    :phone,
+                                    :email,
+                                    :comments,
+                                    :status)
+  end
 end
--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,20 @@
 class MenusController < ApplicationController
+
+  def index; end
+  def show; end
+  def new; end
+  def create; end
+  def edit; end
+  def update; end
+  def destroy; end
+
+  private
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:menu).permit(:content)
+  end
 end

d9c9aa86f2002c2e3774a61387157c1d8897d19b
Antoine Mariani <antoine.mariani@me.com>
2023-01-17 16:58:30 +0100

    Model booking : implementation of callback methods for booking mailings (saved, confirmed, updated, cancelled

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   validates :email, presence: true
 
   after_create :email_saved
-  around_update :email_modified
+  around_update :email_updated
   after_destroy :email_cancelled
 
   private
@@ -16,17 +16,17 @@ class Booking < ApplicationRecord
     BookingMailer.with(booking: self).booking_confirmed
   end
 
-  def email_modified
+  def email_updated
     if self.status_changed? && self.status == "confirmed"
       BookingMailer.with(booking: self).booking_confirmed
-    elsif self.status_changed? && self.status == "cancelled"
-      BookingMailer.with(booking: self).booking_cancelled
+    elsif self.status_changed? && self.status == "declined"
+      BookingMailer.with(booking: self).booking_declined
     else
       BookingMailer.with(booking: self).booking_modified
     end
   end
 
-  def email_declined
-    BookingMailer.with(booking: self).booking_declined
+  def email_cancelled
+    BookingMailer.with(booking: self).booking_cancelled
   end
 end

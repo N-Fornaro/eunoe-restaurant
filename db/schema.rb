09269ed7e62f1bd704b81af892b59c157858e1da
Antoine Mariani <antoine.mariani@me.com>
2023-01-18 17:01:30 +0100

    Adding AdminNotifying in BookingMailer + Default status in booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_140524) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_155623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_140524) do
     t.string "phone"
     t.string "email"
     t.string "comment"
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

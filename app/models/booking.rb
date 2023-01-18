9292cc6d658ed8032ba5b0d2d14ff3c836757e15
Antoine Mariani <antoine.mariani@me.com>
2023-01-18 15:36:51 +0100

    Adding last Booking mailer templates (empty) + Fixing callbacks

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -13,20 +13,20 @@ class Booking < ApplicationRecord
   end
 
   def email_confirmed
-    BookingMailer.with(booking: self).booking_confirmed
+    BookingMailer.with(booking: self).booking_confirmed.deliver!
   end
 
   def email_updated
     if self.status_changed? && self.status == "confirmed"
-      BookingMailer.with(booking: self).booking_confirmed
+      BookingMailer.with(booking: self).booking_confirmed.deliver!
     elsif self.status_changed? && self.status == "declined"
-      BookingMailer.with(booking: self).booking_declined
+      BookingMailer.with(booking: self).booking_declined.deliver!
     else
-      BookingMailer.with(booking: self).booking_modified
+      BookingMailer.with(booking: self).booking_modified.deliver!
     end
   end
 
   def email_cancelled
-    BookingMailer.with(booking: self).booking_cancelled
+    BookingMailer.with(booking: self).booking_cancelled.deliver!
   end
 end

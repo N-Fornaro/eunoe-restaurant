b5b535b6058808ba1bf48dbbddc5d35c90be3e09
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2023-01-24 16:33:30 +0100

    Menu - DB fixed & Seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_151539) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_152522) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_151539) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "price"
+    t.string "meal"
   end
 
   create_table "newsletters", force: :cascade do |t|

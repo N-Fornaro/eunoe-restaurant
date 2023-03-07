7ba8c2ab75a3bd5582fd5cc0dc78e02cc5cfc72f
Antoine Mariani <antoine.mariani@me.com>
2023-03-07 11:12:31 +0100

    admin tabs - done & working with stimulus.js, yet to be filled with content, style and icons

--- app/javascript/controllers/admin_controller.js
+++ app/javascript/controllers/admin_controller.js
@@ -6,9 +6,63 @@ export default class extends Controller {
   connect() {}
 
   tabs(event) {
-    event.currentTarget.innerHTML = `<a href="" class="inline-flex p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500 group" aria-current="page">
-        <svg aria-hidden="true" class="w-5 h-5 mr-2 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>${event.currentTarget.innerText}
-      </a>`;
-    // const currentTab = event.currentTarget.id;
+    event.preventDefault();
+    const activeLinkClasses = [
+      'inline-flex',
+      'p-4',
+      'text-blue-600',
+      'border-b-2',
+      'border-blue-600',
+      'rounded-t-lg',
+      'active',
+      'dark:text-blue-500',
+      'dark:border-blue-500',
+      'group',
+    ];
+    const inactiveLinkClasses = [
+      'inline-flex',
+      'p-4',
+      'border-b-2',
+      'border-transparent',
+      'rounded-t-lg',
+      'hover:text-gray-600',
+      'hover:border-gray-300',
+      'dark:hover:text-gray-300',
+      'group',
+    ];
+    const activeSvgClasses = ['text-blue-600', 'dark:text-blue-500'];
+    const inactiveSvgClasses = [
+      'text-gray-400',
+      'group-hover:text-gray-500',
+      'dark:text-gray-500',
+      'dark:group-hover:text-gray-300',
+    ];
+
+    this.tabTargets.forEach((tab) => {
+      tab.querySelector('a').classList.remove(...activeLinkClasses);
+      tab.querySelector('a').classList.add(...inactiveLinkClasses);
+      tab.querySelector('svg').classList.remove(...activeSvgClasses);
+      tab.querySelector('svg').classList.add(...inactiveSvgClasses);
+    });
+
+    event.currentTarget
+      .querySelector('a')
+      .classList.remove(...inactiveLinkClasses);
+    event.currentTarget.querySelector('a').classList.add(...activeLinkClasses);
+    event.currentTarget
+      .querySelector('svg')
+      .classList.remove(...inactiveSvgClasses);
+    event.currentTarget.querySelector('svg').classList.add(...activeSvgClasses);
+
+    this.tabTargets.forEach((tab) => {
+      document
+        .querySelector(`#active-${tab.id}`)
+        .classList.replace('block', 'hidden');
+    });
+
+    const currentTab = event.currentTarget.id;
+    document
+      .querySelector(`#active-${currentTab}`)
+      .classList.replace('hidden', 'block');
   }
 }

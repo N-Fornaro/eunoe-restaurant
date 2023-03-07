d69d4f0de8ade515ce578371046d88ecad2ac792
Antoine Mariani <antoine.mariani@me.com>
2023-03-07 18:36:31 +0100

    Bookings table -> Searching by date of booking with Filterable + Flatpickr // Next -> Front, More search options, Reset button(s)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,11 +3,14 @@
 class Booking < ApplicationRecord
   validates :email, presence: true
 
-  FILTER_PARAMS = %i[last_name column direction].freeze
+  FILTER_PARAMS = %i[last_name date column direction].freeze
 
   scope :by_last_name, ->(query) { where('bookings.last_name ilike ?', "%#{query}%") }
+  scope :by_date, ->(query) { where(date: query) if query.present? }
 
   def self.filter(filters)
-    Booking.by_last_name(filters['last_name']).order("#{filters['column']} #{filters['direction']}")
+    Booking.by_last_name(filters['last_name'])
+           .by_date(filters['date'])
+           .order("#{filters['column']} #{filters['direction']}")
   end
 end

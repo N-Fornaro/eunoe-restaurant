1c89db61c76dd0946d0e6a3f79bd543565488c3d
Antoine Mariani <antoine.mariani@me.com>
2023-03-08 17:39:56 +0100

    adding fr locales to update date formats in Flatpickr. Still to config for other use

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,10 +1,12 @@
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
+import { French } from 'flatpickr/dist/l10n/fr.js';
 
 const initFlatpickr = () => {
-  flatpickr(".dateTime", {
+  flatpickr('.dateTime', {
     allowInput: true,
-    dateFormat: "d-m-Y",
-    minDate: "today"
+    dateFormat: 'd-m-Y',
+    locale: French,
+    // minDate: "today"
   });
-}
+};
 export { initFlatpickr };

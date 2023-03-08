b7e40ee4807182aeed70258c26d86635d07eb8cf
Antoine Mariani <antoine.mariani@me.com>
2023-03-08 11:47:46 +0100

    Adding filtering by booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,13 +3,15 @@
 class Booking < ApplicationRecord
   validates :email, presence: true
 
-  FILTER_PARAMS = %i[last_name date column direction].freeze
+  FILTER_PARAMS = %i[last_name status date column direction].freeze
 
   scope :by_last_name, ->(last_name) { where('bookings.last_name ilike ?', "%#{last_name}%") if last_name.present? }
+  scope :by_status, ->(status) { where(status:) if status.present? }
   scope :by_date, ->(date) { where(date:) if date.present? }
 
   def self.filter(filters)
     Booking.by_last_name(filters['last_name'])
+           .by_status(filters['status'])
            .by_date(filters['date'])
            .order("#{filters['column']} #{filters['direction']}")
   end

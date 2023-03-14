1411bfc6211cb0effe3fcae09d30d7a35690704a
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2023-03-14 18:51:32 +0100

    Add 3 Typo client to use

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 Rails.application.config.assets.enabled = true
 
 # Precompile additional assets.

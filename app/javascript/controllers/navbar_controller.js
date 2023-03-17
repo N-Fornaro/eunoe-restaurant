e92b03a2c4dbcb841b7b042ed61da6dadd162505
Antoine Mariani <antoine.mariani@me.com>
2023-03-17 14:15:59 +0100

    Navbar upgraded & fixed

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,8 +2,28 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ['burger', 'button'];
-  connect() {}
+  static targets = ['burger', 'button', 'navbar', 'booking'];
+  static classes = ['background', 'bookingButtonBackground'];
+
+  connect() {
+    if (
+      ['', 'restaurant', 'privatisation', 'meals', 'bookings/new'].includes(
+        window.location.pathname.substring(
+          window.location.pathname.length - 1
+        ) === '/'
+          ? window.location.pathname.substring(
+              4,
+              window.location.pathname.length - 1
+            )
+          : window.location.pathname.substring(4)
+      )
+    ) {
+    } else {
+      this.navbarTarget.classList.add(...this.backgroundClasses);
+      this.bookingTarget.classList.remove('bg-pink-pastel', 'text-white');
+      this.bookingTarget.classList.add(...this.bookingButtonBackgroundClasses);
+    }
+  }
 
   burgerMenu(event) {
     if (event.currentTarget.id === 'burger-open') {
@@ -16,6 +36,35 @@ export default class extends Controller {
       event.currentTarget.id = 'burger-open';
       this.buttonTarget.innerHTML =
         "<svg xmlns='http://www.w3.org/2000/svg' class='w-5 h-5' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M4 6h16M4 12h16M4 18h16'/></svg>";
+    }
+  }
+
+  backgroundHandler(event) {
+    if (
+      ['', 'restaurant', 'privatisation', 'bookings/new'].includes(
+        window.location.pathname.substring(
+          window.location.pathname.length - 1
+        ) === '/'
+          ? window.location.pathname.substring(
+              4,
+              window.location.pathname.length - 1
+            )
+          : window.location.pathname.substring(4)
+      )
+    ) {
+      if (window.scrollY > 100) {
+        this.navbarTarget.classList.add(...this.backgroundClasses);
+        this.bookingTarget.classList.remove('bg-pink-pastel', 'text-white');
+        this.bookingTarget.classList.add(
+          ...this.bookingButtonBackgroundClasses
+        );
+      } else {
+        this.navbarTarget.classList.remove(...this.backgroundClasses);
+        this.bookingTarget.classList.remove(
+          ...this.bookingButtonBackgroundClasses
+        );
+        this.bookingTarget.classList.add('bg-pink-pastel', 'text-white');
+      }
     }
   }
 }

959692b4c25e29d6ba509f2f3ae2923c7fb8928d
Antoine Mariani <antoine.mariani@me.com>
2023-04-03 11:19:08 +0200

    FIX - Commented ErrorsController deprecated tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get not_found" do
-    get errors_not_found_url
-    assert_response :success
-  end
+  # test "should get not_found" do
+  #   visit errors_not_found_url
+  #   assert_response :success
+  # end
 
-  test "should get internal_server_error" do
-    get errors_internal_server_error_url
-    assert_response :success
-  end
+  # test "should get internal_server_error" do
+  #   visit errors_internal_server_error_url
+  #   assert_response :success
+  # end
 end

8402681f6d9d03c874b0a24bee276a4cf753d7f7
Nicolas Fornaro <nicolasfornaro75@gmail.com>
2023-06-28 11:57:24 +0200

    No Monday to flatpickr Booking

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -15,6 +15,15 @@ const initFlatpickr = () => {
     dateFormat: 'd-m-Y',
     locale: French,
     minDate: 'today',
+    disable: [
+        function(date) {
+            // return true to disable
+            return date.getDay() === 1
+        },
+    ],
+    // "locale": {
+    //     "firstDayOfWeek": 1 // start week on Monday
+    //   }
   });
 };
 export { initFlatpickr };
